fda60a50a77f2805cc35d168b26faadbcb7db678
Sander de Leeuw <s.deleeuw@gmail.com>
2026-08-25 20:40:59 +0000

    ask for luks passphrase during boot

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices.crypted.device = "/dev/disk/by-uuid/838dee72-0e92-40bc-a08a-e59055930dd2";
 
   networking.networkmanager.enable = true;
 
