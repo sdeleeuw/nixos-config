33d5fdaea7cc864c6c5f3bfd394e6db2775739dc
Sander de Leeuw <s.deleeuw@gmail.com>
2026-08-25 20:52:32 +0000

    set hostname

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.luks.devices.crypted.device = "/dev/disk/by-uuid/838dee72-0e92-40bc-a08a-e59055930dd2";
 
+  networking.hostName = "infinity";
   networking.networkmanager.enable = true;
 
   users.users = {
