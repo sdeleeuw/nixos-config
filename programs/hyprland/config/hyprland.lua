3b6010c4996b2754d6bedb52c4c43c0da6d3544b
Sander de Leeuw <s.deleeuw@gmail.com>
2026-08-29 23:28:16 +0200

    update border color in hyprland

--- programs/hyprland/config/hyprland.lua
+++ programs/hyprland/config/hyprland.lua
@@ -103,7 +103,7 @@ hl.config({
         border_size = 2,
 
         col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+            active_border   = { colors = {"rgba(5e81acff)", "rgba(81a1c1ff)"}, angle = 45 },
             inactive_border = "rgba(595959aa)",
         },
 
